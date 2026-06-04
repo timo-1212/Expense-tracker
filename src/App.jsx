dd5c9efdeeea828886ddc9e1379b919ed22ecaf7
timo <xatovit00@stud.fit.vutbr.cz>
2026-06-04 10:36:55 +0200

    Add transaction form and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { useState } from 'react'
 import Balance from './components/Balance'
 import TransactionForm from './components/TransactionForm'
 import TransactionList from './components/TransactionList'
 
 function App() {
 
-  const transactions = [
+  const [transactions, setTransactions] = useState([
     { id: 1, title: "Lunch", amount: -8 },
     { id: 2, title: "Salary", amount: 500},
-  ]
+  ])
+
+  function addTransaction(transaction) {
+    setTransactions([...transactions, transaction])
+  }
 
   return (
     <>
       <h1>Expense Tracker</h1>
 
       <Balance transactions={transactions} />
-      <TransactionForm />
+      <TransactionForm addTransaction={addTransaction} />
       <TransactionList transactions={transactions} />
     </>
   )
--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,5 +1,42 @@
-function TransactionForm () {
-    return <p>Transaction Form</p>
+import { useState } from "react"
+
+function TransactionForm ({ addTransaction }) {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const newTransaction = {
+            id: Date.now(),
+            title: title,
+            amount: Number(amount),
+        }
+
+        addTransaction(newTransaction)
+
+        setTitle('')
+        setAmount('')
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Transaction title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+            />
+
+            <input
+                type="number"
+                placeholder="Amount"
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
+            />
+
+            <button type="submit">Add transaction</button>
+        </form>
+    )
 }
 
 export default TransactionForm
